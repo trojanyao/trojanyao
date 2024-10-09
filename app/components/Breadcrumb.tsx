c10082537c70a342d3d5389235a35403c5433a75
TROJAN <ytj1996@gmail.com>
2024-10-09 15:30:06 +0800

    feat: add Breadcrumb component

---
+++ app/components/Breadcrumb.tsx
@@ -0,0 +1,35 @@
+import { HomeIcon } from '@heroicons/react/16/solid';
+import { ChevronRightIcon } from '@heroicons/react/20/solid';
+import Link from 'next/link';
+import { Fragment } from 'react';
+
+interface MenuItem {
+  text: string;
+  url: string;
+}
+
+export default function Breadcrumb({ menus }: { menus: MenuItem[] }) {
+  return (
+    <div className="pt-4 pb-8 flex items-center gap-2">
+      <Link href="/">
+        <HomeIcon className="size-4 text-light" />
+      </Link>
+
+      <div className="flex items-center gap-1">
+        {menus?.map((menu: MenuItem, index: number) => (
+          <Fragment key={index.toString()}>
+            <ChevronRightIcon className="size-4 text-light" />
+            <Link
+              href={menu?.url}
+              className={`${
+                index === menus?.length - 1 ? 'text-secondary' : 'text-light'
+              } text-small hover:text-secondary`}
+            >
+              {menu?.text}
+            </Link>
+          </Fragment>
+        ))}
+      </div>
+    </div>
+  );
+}
