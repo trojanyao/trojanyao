e3b1c2439a2ac39c28af9d9ae08698b6f1b6b344
TROJAN <ytj1996@gmail.com>
2024-10-18 12:03:55 +0800

    feat(Skill): add skill list page

--- app/dev/page.tsx
+++ app/dev/page.tsx
@@ -37,7 +37,7 @@ export default function Develop() {
       {/* Develop Skills */}
       <section className="section-item">
         <SectionHeader
-          url="/dev/skills"
+          url="/skill/dev"
           icon={<CodeBracketSquareIcon />}
           title="开发技能"
         />
---
+++ app/skill/[type]/page.tsx
@@ -0,0 +1,44 @@
+import Breadcrumb from '@/app/components/Breadcrumb';
+import GroupBy from '@/app/components/GroupBy';
+import Line from '@/app/components/Line';
+import SectionHeader from '@/app/components/SectionHeader';
+import {
+  CodeBracketSquareIcon,
+  FolderOpenIcon,
+  RectangleStackIcon,
+} from '@heroicons/react/24/outline';
+import SkillGrid from '../SkillGrid';
+
+const breadcrumbMenus = [
+  { text: '开发', url: '/dev' },
+  { text: '开发技能', url: '/dev/projects' },
+];
+
+const groupByOptions = [
+  { icon: <RectangleStackIcon />, text: '按熟练度' },
+  { icon: <FolderOpenIcon />, text: '按类别' },
+];
+
+export default function DevProjects() {
+  return (
+    <div>
+      <Breadcrumb menus={breadcrumbMenus} />
+
+      <SectionHeader title="开发技能" icon={<CodeBracketSquareIcon />}>
+        <GroupBy options={groupByOptions} />
+      </SectionHeader>
+
+      <div className="flex flex-col gap-6">
+        <Line type="second" />
+
+        {/* List */}
+        {Array.from({ length: 3 }).map((item, index) => (
+          <div key={index} className="flex flex-col gap-4">
+            <div className="title-small text-secondary">学习中</div>
+            <SkillGrid />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
