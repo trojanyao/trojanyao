d5d2007adc13b7c7927e909001ec6687d37e0495
TROJAN <ytj1996@gmail.com>
2024-11-12 15:47:43 +0800

    feat(Home): add selected projects getting logic

--- app/components/SectionProject.tsx
+++ app/components/SectionProject.tsx
@@ -1,20 +1,19 @@
 import { CheckBadgeIcon } from '@heroicons/react/24/outline';
 
+import ProjectItem from '../project/components/ProjectItem';
 import SectionHeader from './SectionHeader';
-import ProjectItem from '../project/ProjectItem';
+import { getProjects } from '@/lib/notion';
 
-export default function SectionProject() {
+export default async function SectionProject() {
+  const projects = await getProjects([{ property: '首页精选', checkbox: { equals: true } }]);
+
   return (
     <section>
-      <SectionHeader
-        url="/project"
-        title="精选项目"
-        icon={<CheckBadgeIcon />}
-      />
+      <SectionHeader url="/project" title="精选项目" icon={<CheckBadgeIcon />} />
 
       <div className="max-w-full flex gap-6 overflow-x-scroll">
-        {Array.from({ length: 6 }).map((item, index) => (
-          <ProjectItem key={index} />
+        {projects.map((item, index) => (
+          <ProjectItem key={index} data={item} />
         ))}
       </div>
     </section>
