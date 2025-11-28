931a11e5a954cd3dd98e4c004bb856d39c72ef3e
trojan.yao <ytj1996@gmail.com>
2025-11-28 10:34:57 +0800

    feat(skill/type): add page

--- app/skill/[type]/page.tsx
+++ app/skill/[type]/page.tsx
@@ -16,28 +16,29 @@ const breadcrumbMenus = [
   { text: '开发技能', url: '/dev/projects' },
 ];
 
-const groupByOptions = [
-  { icon: <RectangleStackIcon />, text: '按熟练度' },
-  { icon: <FolderOpenIcon />, text: '按类别' },
+const groupByOptions: GroupOptionItem[] = [
+  { icon: <RectangleStackIcon />, key: 'proficiency', text: '按熟练度' },
+  { icon: <FolderOpenIcon />, key: 'category', text: '按类别' },
 ];
 
+// TODO: 整个页面需要开发
 export default function DevProjects() {
   return (
     <div>
       <Breadcrumb menus={breadcrumbMenus} />
 
       <SectionHeader title="开发技能" icon={<CodeBracketSquareIcon />}>
-        <GroupBy options={groupByOptions} />
+        {/* <GroupBy options={groupByOptions} /> */}
       </SectionHeader>
 
       <div className="flex flex-col gap-6">
-        <Line type="second" />
+        <Line type="secondary" />
 
         {/* List */}
         {Array.from({ length: 3 }).map((item, index) => (
           <div key={index} className="flex flex-col gap-4">
             <div className="title-small text-secondary">学习中</div>
-            <SkillGrid />
+            {/* <SkillGrid /> */}
           </div>
         ))}
       </div>
