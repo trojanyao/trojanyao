8929fcdc9ea3ae87daadc80c69b3730b7c3e5750
trojan.yao <ytj1996@gmail.com>
2025-12-04 15:30:35 +0800

    feat(Breadcrumb): update styles

--- app/components/ui/Breadcrumb.tsx
+++ app/components/ui/Breadcrumb.tsx
@@ -1,8 +1,8 @@
 import { Fragment } from 'react';
 import Link from 'next/link';
 
-import { HomeIcon } from '@heroicons/react/16/solid';
 import { ChevronRightIcon } from '@heroicons/react/20/solid';
+import { HomeIcon } from '@heroicons/react/24/outline';
 
 interface MenuItem {
   text: string;
@@ -11,25 +11,23 @@ interface MenuItem {
 
 export default function Breadcrumb({ menus }: { menus: MenuItem[] }) {
   return (
-    <div className="pt-4 pb-8 flex items-center gap-2">
+    <div className="pt-4 pb-8 flex items-center gap-1">
       <Link href="/">
         <HomeIcon className="size-4 text-light" />
       </Link>
 
-      <div className="flex items-center gap-1">
-        {menus?.map((menu: MenuItem, index: number) => (
-          <Fragment key={index.toString()}>
-            <ChevronRightIcon className="size-4 text-light" />
-            <div
-              className={`${
-                index === menus?.length - 1 ? 'text-secondary' : 'text-light'
-              } text-small ${menu?.url ? 'hover:text-secondary' : ''}`}
-            >
-              {menu?.url ? <Link href={menu?.url}>{menu?.text}</Link> : <span>{menu?.text}</span>}
-            </div>
-          </Fragment>
-        ))}
-      </div>
+      {menus?.map((menu: MenuItem, index: number) => (
+        <Fragment key={index.toString()}>
+          <ChevronRightIcon className="size-4 text-light" />
+          <div
+            className={`${
+              index === menus?.length - 1 ? 'text-secondary' : 'text-light'
+            } text-small ${menu?.url ? 'hover:text-secondary' : ''}`}
+          >
+            {menu?.url ? <Link href={menu?.url}>{menu?.text}</Link> : <span>{menu?.text}</span>}
+          </div>
+        </Fragment>
+      ))}
     </div>
   );
 }
