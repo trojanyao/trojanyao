855698fcf672f5427502b4ef8dff61215024a805
trojan.yao <ytj1996@gmail.com>
2025-12-10 14:36:13 +0800

    build: replace `next lint` with `eslint --fix`

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,7 @@
 const path = require('path');
 
 const buildEslintCommand = (filenames) =>
-  `next lint --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' --file ')}`;
+  `eslint --fix ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')}`;
 
 module.exports = {
   '*.{js,jsx,ts,tsx}': [buildEslintCommand],
