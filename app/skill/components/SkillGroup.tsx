9d90c09eddcd2d032a1a4f6e56f9168f3a40ea95
trojan.yao <ytj1996@gmail.com>
2026-01-12 11:49:18 +0800

    refactor(skill/SkillGroup): refactor grouping logic

--- app/skill/components/SkillGroup.tsx
+++ app/skill/components/SkillGroup.tsx
@@ -29,14 +29,22 @@ export default function SkillGroup({ skills }: { skills: Skill[] }) {
     // 自定义分组顺序
     // 按 status（熟练度）分组时按照 sillStatuses 顺序（学习中 - 熟练 - 使用过）
     // 按 category（类别）分组时按照 skillCategories 顺序（前端 - 服务端 - App - 其他）
-    const indexA =
-      groupKey === 'status'
-        ? skillStatuses.indexOf(a?.groupName as SkillStatus)
-        : skillCategories.indexOf(a?.groupName as SkillCategory);
-    const indexB =
-      groupKey === 'status'
-        ? skillStatuses.indexOf(b.groupName as SkillStatus)
-        : skillCategories.indexOf(b?.groupName as SkillCategory);
+
+    let getIndex: (item: typeof a) => number;
+
+    if (groupKey === 'status') {
+      // 当 groupKey 为 'status' 时，使用 skillStatuses 进行排序
+      getIndex = (item) => skillStatuses.indexOf(item?.groupName as SkillStatus);
+    } else {
+      // 当 groupKey 为 'category' 时，使用 skillCategories 进行排序
+      getIndex = (item) => skillCategories.indexOf(item?.groupName as SkillCategory);
+    }
+
+    const indexA = getIndex(a);
+    const indexB = getIndex(b);
+
+    // 如果未找到索引，则将其视为 Infinity，以确保它们排在最后
+    // 否则按索引顺序排序
     return (indexA === -1 ? Infinity : indexA) - (indexB === -1 ? Infinity : indexB);
   });
 
