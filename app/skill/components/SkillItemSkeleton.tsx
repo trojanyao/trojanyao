68f8706a0e2875de89062ddcb7fd4a68f042d381
trojan.yao <ytj1996@gmail.com>
2026-01-28 16:34:03 +0800

    feat(skill/list): add skeleton

---
+++ app/skill/components/SkillItemSkeleton.tsx
@@ -0,0 +1,15 @@
+export default function SkillItemSkeleton() {
+  return (
+    <div className="bg-light-gray p-4 rounded-2xl animate-pulse flex items-center gap-3">
+      {/* Logo */}
+      <div className="size-10 bg-gray-100 rounded-lg"></div>
+
+      {/* Name + Status */}
+      <div className="flex-1 h-full py-[2px] flex flex-col justify-between overflow-hidden">
+        <div className="w-4/5 h-4 bg-gray-100 rounded-md"></div>
+
+        <div className="w-12 h-3 bg-gray-100 rounded-full"></div>
+      </div>
+    </div>
+  );
+}
