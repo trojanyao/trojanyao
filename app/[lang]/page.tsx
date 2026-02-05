0ff5db0e7564e9b8a1620e827b2850011bbb558a
trojan.yao <ytj1996@gmail.com>
2026-02-05 11:15:35 +0800

    Revert "feat(i18n): add proxy(middleware), `dictionaries.ts` context and JSON dictionaries to handle i18n logic"
    This reverts commit 4d939e32f285ea3de259e8bee335cced534a7c2e.

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,16 +1,11 @@
 // import SectionService from './components/SectionService';
-import { getDictionary, Locale } from '@/app/i18n/dictionaries';
-
-import Banner from './components/section/Banner';
-import SectionProject from './components/section/SectionProject';
-import SectionSkill from './components/section/SectionSkill';
+import Banner from '../components/section/Banner';
+import SectionProject from '../components/section/SectionProject';
+import SectionSkill from '../components/section/SectionSkill';
 
 export const dynamic = 'force-dynamic'; // use SSR to avoid Notion's image expiry
 
-export default async function Home({ params }: PageProps<'/[lang]'>) {
-  const { lang } = await params;
-  const dict = await getDictionary(lang as Locale);
-
+export default function Home() {
   return (
     <div className="home flex flex-col items-center">
       {/* Banner */}
@@ -19,7 +14,7 @@ export default async function Home({ params }: PageProps<'/[lang]'>) {
       {/* Content */}
       <div className="max-w-[1200px] mx-auto p-6 md:p-8 xl:px-0 box-border flex flex-col gap-6">
         {/* <SectionService /> */}
-        <SectionProject dict={dict} />
+        <SectionProject />
         <SectionSkill />
       </div>
     </div>
