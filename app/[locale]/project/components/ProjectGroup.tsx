5a33c254fd3dc93689e48a47674584c74a10e952
trojan.yao <ytj1996@gmail.com>
2026-02-11 11:22:02 +0800

    feat(project/list): add localization

--- app/[locale]/project/components/ProjectGroup.tsx
+++ app/[locale]/project/components/ProjectGroup.tsx
@@ -8,6 +8,7 @@ import {
   RectangleGroupIcon,
   Squares2X2Icon,
 } from '@heroicons/react/24/outline';
+import { useTranslations } from 'next-intl';
 
 import SectionHeader from '@/app/[locale]/components/common/SectionHeader';
 import GroupBy from '@/app/[locale]/components/ui/GroupBy';
@@ -17,12 +18,14 @@ import ProjectGrid from '@/app/[locale]/project/components/ProjectGrid';
 import { ProjectPlatform } from '@/lib/constants/project.constants';
 import { groupBy } from '@/lib/utils/group-by';
 
-const groupByOptions = [
-  { icon: <ClockIcon />, text: '按时间', key: 'dateStart' },
-  { icon: <RectangleGroupIcon />, text: '按形态', key: 'platform' },
-];
+export default function ProjectGroup({ projects, title }: { projects: Project[]; title?: string }) {
+  const t = useTranslations('project');
 
-export default function ProjectList({ projects, title }: { projects: Project[]; title?: string }) {
+  const groupByOptions = [
+    { icon: <ClockIcon />, text: t('group.by-time'), key: 'dateStart' },
+    { icon: <RectangleGroupIcon />, text: t('group.by-platform'), key: 'platform' },
+  ];
+
   const [groupKey, setGroupKey] = useState<keyof Project>('dateStart');
 
   const projectPlatformValues: string[] = Object.values(ProjectPlatform);
@@ -42,20 +45,28 @@ export default function ProjectList({ projects, title }: { projects: Project[]; 
           return (
             new Date(b.items[0].dateStart).getTime() - new Date(a.items[0].dateStart).getTime()
           );
-        }
+        },
   );
+  // 按平台 group 时，给 groupName 使用 localization
+  const localizedGroupedProjects =
+    groupKey === 'platform'
+      ? groupedProjects.map((groupItem) => ({
+          ...groupItem,
+          groupName: t?.(`type.${groupItem.groupName}`) || groupItem.groupName,
+        }))
+      : groupedProjects;
 
   /* Count total number of projects and remove duplicates */
   const allProjects = groupedProjects.flatMap((group) => group?.items || []);
   const uniqueProjects = Array.from(new Set(allProjects.map((item) => item.id))).map((id) =>
-    allProjects.find((item) => item.id === id)
+    allProjects.find((item) => item.id === id),
   );
   const totalProjects = uniqueProjects.length;
 
   return (
     <div>
       <SectionHeader
-        title={`${title || '开发项目'}`}
+        title={`${title || t('dev-project')}`}
         count={totalProjects}
         icon={<Squares2X2Icon />}
       >
@@ -73,7 +84,7 @@ export default function ProjectList({ projects, title }: { projects: Project[]; 
 
         {/* List */}
         {totalProjects > 0 ? (
-          groupedProjects.map((groupItem, index) => (
+          localizedGroupedProjects.map((groupItem, index) => (
             <div key={index} className="flex flex-col gap-4">
               <div className="flex items-center gap-1 title-small text-secondary">
                 <div>{groupItem?.groupName}</div>
--- app/[locale]/project/page.tsx
+++ app/[locale]/project/page.tsx
@@ -2,6 +2,8 @@ import { Suspense } from 'react';
 
 import { Squares2X2Icon } from '@heroicons/react/24/outline';
 import { Metadata } from 'next';
+import { useTranslations } from 'next-intl';
+import { getTranslations } from 'next-intl/server';
 
 import Breadcrumb from '@/app/[locale]/components/ui/Breadcrumb';
 import { getProjects } from '@/lib/notion';
@@ -9,21 +11,35 @@ import { getProjects } from '@/lib/notion';
 import SectionHeader from '../components/common/SectionHeader';
 import Line from '../components/ui/Line';
 
-import ProjectList from './components/ProjectGroup';
+import ProjectGroup from './components/ProjectGroup';
 import ProjectCardSkeleton from './components/ProjectItemSkeleton';
 
 export const dynamic = 'force-dynamic'; // use SSR to avoid Notion's image expiry
 
-export const metadata: Metadata = {
-  title: '开发项目',
-  description: '参与和主导的历史开发项目案例，包括 Web、App、小程序等。',
-};
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}): Promise<Metadata> {
+  const { locale } = await params;
+  const t = await getTranslations({ locale, namespace: 'project' });
+
+  return {
+    title: t('dev-project'),
+    description:
+      locale === 'zh'
+        ? '主导和参与的开发项目案例，包括 Web、App、小程序等。'
+        : 'Development project cases that I have led or participated in, including Web, App, Mini Programs, etc.',
+  };
+}
 
 export default function DevProjects() {
+  const t = useTranslations('dev');
+
   const breadcrumbMenus = [
-    { text: '开发' }, // TODO: open /dev url
+    { text: t('common') }, // TODO: open /dev url
     // { text: '开发', url: '/dev' },
-    { text: '开发项目', url: '/project' },
+    { text: t('dev-project'), url: '/project' },
   ];
 
   return (
@@ -41,15 +57,17 @@ export default function DevProjects() {
 async function ProjectsContent() {
   const projects = await getProjects();
 
-  return <ProjectList projects={projects} />;
+  return <ProjectGroup projects={projects} />;
 }
 
 function ProjectListSkeleton() {
+  const t = useTranslations('dev');
+
   const skeletonArray = Array.from({ length: 3 });
 
   return (
     <div>
-      <SectionHeader title="开发项目" icon={<Squares2X2Icon />}>
+      <SectionHeader title={t('dev-project')} icon={<Squares2X2Icon />}>
         {/* Avoid CLS */}
         <div className="h-7"></div>
       </SectionHeader>
