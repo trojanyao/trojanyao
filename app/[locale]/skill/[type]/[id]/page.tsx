485105bdb0ea90255b4d93da330c111cba2b1a31
trojan.yao <ytj1996@gmail.com>
2026-02-11 12:41:34 +0800

    feat(skill/detail): add localization

--- app/[locale]/skill/[type]/[id]/page.tsx
+++ app/[locale]/skill/[type]/[id]/page.tsx
@@ -4,6 +4,8 @@ import Link from 'next/link';
 
 import { ArrowTopRightOnSquareIcon } from '@heroicons/react/16/solid';
 import { Metadata } from 'next';
+import { useLocale } from 'next-intl';
+import { getLocale, getTranslations } from 'next-intl/server';
 
 import Breadcrumb from '@/app/[locale]/components/ui/Breadcrumb';
 import ProjectList from '@/app/[locale]/project/components/ProjectGroup';
@@ -15,13 +17,13 @@ import SkillStatus from '../../components/SkillStatus';
 export async function generateMetadata({
   params,
 }: {
-  params: Promise<{ id: string }>;
+  params: Promise<{ locale: string; id: string }>;
 }): Promise<Metadata> {
-  const { id } = await params;
+  const { locale, id } = await params;
   const skill = await getSkill(id);
 
   return {
-    title: skill?.name,
+    title: locale === 'zh' ? skill?.name : skill?.nameEN || skill?.name,
     description: skill?.description,
   };
 }
@@ -41,18 +43,23 @@ export default async function SkillDetail({ params }: { params: Promise<{ id: st
 }
 
 async function SkillBreadcrumb({ dataPromise }: { dataPromise: Promise<Skill> }) {
+  const locale = await getLocale();
+  const t = await getTranslations();
+
   const skill = await dataPromise;
 
   const breadcrumbMenus = [
-    { text: '开发', url: '/dev' },
-    { text: '开发技能', url: '/skill/dev' },
-    { text: skill?.name },
+    { text: t('common.dev'), url: '/dev' },
+    { text: t('skill.dev-skill'), url: '/skill/dev' },
+    { text: locale === 'en' ? skill?.nameEN || skill?.name : skill?.name },
   ];
 
   return <Breadcrumb menus={breadcrumbMenus} />;
 }
 
 async function SkillContent({ dataPromise }: { dataPromise: Promise<Skill> }) {
+  const t = await getTranslations();
+
   const skill = await dataPromise;
 
   /* <RelatedProjects> must be a client component, so we need to fetch the projects here */
@@ -69,12 +76,14 @@ async function SkillContent({ dataPromise }: { dataPromise: Promise<Skill> }) {
     <div className="flex flex-col gap-8">
       <BasicInfo skill={skill} />
 
-      <ProjectList projects={projects} title="相关项目" />
+      <ProjectList projects={projects} title={t('skill.related-project')} />
     </div>
   );
 }
 
 function BasicInfo({ skill }: { skill: Skill }) {
+  const locale = useLocale();
+
   return (
     <div className="flex justify-between items-center">
       {/* Left */}
@@ -84,7 +93,9 @@ function BasicInfo({ skill }: { skill: Skill }) {
         <div className="py-1 flex flex-col justify-center gap-2">
           {/* Name & Link */}
           <div className="flex items-center gap-3">
-            <h1 className="title-large">{skill?.name}</h1>
+            <h1 className="title-large">
+              {locale === 'en' ? skill?.nameEN || skill?.name : skill?.name}
+            </h1>
 
             {skill?.site && (
               <Link href={skill?.site} target="_blank" className="p-1 group">
@@ -94,7 +105,7 @@ function BasicInfo({ skill }: { skill: Skill }) {
           </div>
 
           {/* Desc */}
-          {skill?.description && (
+          {skill?.description && locale === 'zh' && (
             <div className="text-small text-light text-pretty leading-normal">
               {skill?.description}
             </div>
