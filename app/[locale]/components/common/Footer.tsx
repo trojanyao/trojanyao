cb53c31c18535050243ce8658bec0b6b576929fe
trojan.yao <ytj1996@gmail.com>
2026-02-13 10:19:59 +0800

    feat(Footer): add `LocaleSwitcher` component

--- app/[locale]/components/common/Footer.tsx
+++ app/[locale]/components/common/Footer.tsx
@@ -1,11 +1,29 @@
+'use client';
+
+import { LocaleSwitcher } from './locale-switch';
+
 export default function Footer() {
   return (
-    <footer className="w-full mx-auto py-8 border-t border-secondary flex justify-center items-center text-sm leading-none">
-      {/* eslint-disable-next-line no-irregular-whitespace */}
-      <div className="text-light">© 2024 极简一生 All Rights Reserved.</div>
+    <footer
+      className="w-full mx-auto py-6 px-4 xl:px-0 border-t border-secondary
+    flex flex-col sm:flex-row justify-between items-center gap-6
+    text-sm leading-none"
+    >
+      <div className="flex-1">
+        <LocaleSwitcher />
+      </div>
+
+      {/* Center */}
+      <div className="flex-1 text-center text-light whitespace-nowrap">
+        {/* eslint-disable-next-line no-irregular-whitespace */}
+        © 2024–{new Date().getFullYear()} 极简一生 All Rights Reserved.
+      </div>
+
+      {/* Right */}
       {/* <div>
         你是第<span className="mx-1 text-primary font-medium">12</span>位访客
-      </div> */}
+        </div> */}
+      <div className="flex-1"></div>
     </footer>
   );
 }
